87ce06ec1e222c505747dcb7e70146c6cffc2db4
Sardor <22@.ru>
2022-02-27 20:38:59 +0500

    first

---
+++ main.js
@@ -0,0 +1,21 @@
+var Name = prompt('Введите свое имя');
+
+console.log('Ваше имя: ' + Name);
+
+var age = prompt('Введите свой возраст')
+console.log('Ваш возраст: ' + age)
+
+var number1 = prompt('6+12')
+console.log('Правильный ответ = 18 ' + "Ваш ответ " + number1)
+
+var number2 = prompt('16*12')
+console.log('Правильный ответ  192 ' + "Ваш ответ " + number2)
+
+var number3 = prompt('365/5')
+console.log('Правильный ответ = 73 ' + "Ваш ответ " + number3)
+
+var number4 = prompt('Найдие Остаток 7 % 5')
+console.log("правильный ответ 2 " + "Ваш ответ " + number4)
+
+var text = ("Зайдите в Консоль и проверьте свои ответы")
+alert(text)
